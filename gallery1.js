e7cf534222e1a347e6977ae1917bb236d2141056
erkirhann <134487751+erkirhann@users.noreply.github.com>
2023-08-18 10:22:01 +0530

    MOBILE RESPONSIVE GALLERY

--- gallery1.js
+++ gallery1.js
@@ -7,3 +7,21 @@ const swiper = new Swiper('.swiper-container', {
       onlyInViewport: false
     }
   });
+  // Add these modifications to your existing JavaScript code
+const swiper = new Swiper('.swiper-container', {
+  direction: 'vertical',
+  mousewheel: {},
+  effect: 'slide', // Change effect to 'slide' for better mobile performance
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false
+  },
+  slidesPerView: 'auto', // Allow multiple slides per view on mobile
+  spaceBetween: 10, // Add spacing between slides
+  breakpoints: {
+    768: {
+      slidesPerView: 1, // Change to 1 slide per view on screens <= 768px width
+      spaceBetween: 20 // Increase spacing between slides for better readability
+    }
+  }
+});
